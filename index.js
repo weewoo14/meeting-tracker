893a0193681d81046f04ec2772f95a7a217f02f4
Owen Wu <owenwu@Owens-MacBook-Air.local>
2025-03-28 18:39:05 -0400

    Fixed some things

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
+		const command = require(filePath)
 		// Set a new item in the Collection with the key as the command name and the value as the exported module
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
