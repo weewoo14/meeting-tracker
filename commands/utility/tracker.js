76ae564b2f61bf8a1d3e92e7f3a95ed73e9ed012
Owen Wu <owenwu@Owens-MacBook-Air.local>
2025-03-28 18:48:13 -0400

    Added tracker command file

---
+++ commands/utility/tracker.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('quote')
+        .setDescription('Adds the replied message to a new channel'),
+    async execute(interaction){
+        await interaction.reply("Hello World!")
+    }
+
+}
